492a9c25ea28f43503890720cc4d2641b3cae02a
Ahmed Ehab <138992100+AhmedEhabAli@users.noreply.github.com>
2025-10-03 17:57:49 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,58 @@
+var randomQuote = [
+  {
+    paragraph: "“So many books, so little time.”",
+    authorName: "--Frank Zappa",
+  },
+  {
+    paragraph: "“Be yourself; everyone else is already taken.”",
+    authorName: "--Oscar Wilde",
+  },
+  {
+    paragraph: "“A room without books is like a body without a soul.”",
+    authorName: "--Marcus Tullius Cicero",
+  },
+  {
+    paragraph: "“You only live once, but if you do it right, once is enough.”",
+    authorName: "--Mae West",
+  },
+  {
+    paragraph: "“If you tell the truth, you don't have to remember anything.”",
+    authorName: "--Mark Twain",
+  },
+  {
+    paragraph: "“Always forgive your enemies; nothing annoys them so much.”",
+    authorName: "--Oscar Wilde",
+  },
+  {
+    paragraph:
+      "“It is better to be hated for what you are than to be loved for what you are not.”",
+    authorName: "--Andre Gide, Autumn Leaves",
+  },
+  {
+    paragraph: "“We accept the love we think we deserve.”",
+    authorName: "--Stephen Chbosky, The Perks of Being a Wallflower",
+  },
+  {
+    paragraph:
+      "“Live as if you were to die tomorrow. Learn as if you were to live forever.”",
+    authorName: "--Mahatma Gandhi",
+  },
+  {
+    paragraph:
+      "“A friend is someone who knows all about you and still loves you.”",
+    authorName: "--Elbert Hubbard",
+  },
+];
+
+var storage;
+
+function quote() {
+  var x = Math.floor(Math.random() * randomQuote.length);
+  if (storage === x) {
+    return quote();
+  }
+  document.getElementById("paragraph").innerHTML = randomQuote[x].paragraph;
+  document.getElementById("authorName").innerHTML = randomQuote[x].authorName;
+  storage = x;
+  return x;
+}
